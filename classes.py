09bedaeb5db0322b258d17965a90c32ed4b7b0e5
Fr13drich <frederic.saelens@gmail.com>
2025-02-11 14:11:30 +0100

    remove unnecessary-dunder-call

--- classes.py
+++ classes.py
@@ -2,30 +2,37 @@
 from collections import Counter
 from typing import List
 import math
+import requests
+import lxml.etree as ET
+from parsel import Selector
 
 class Recipe():
     """A cooking recipe"""
-    unit_list = ['tour de moulin', 'branches', 'branche', 'feuilles', 'feuille', 'zeste', 'brins', 'brin', 'botte', 'gousses', 'gousse', 'g', 'kg', 'ml', 'cuil. à café', 'cuil. à soupe', 'pincee', 'pincée', 'tête', 'l']
-    juxtaposant_list = ['de', 'd\'', 'à ']
-    def __init__(self, ref: str, name: str, ingredients_bill: dict, instructions=None):
+    unit_list = ['tour de moulin', 'branches', 'branche', 'feuilles', 'feuille', 'zeste', 'brins', 'brin', 'botte', 'gousses', 'gousse', 'g', 'kg', 'ml', 'cuil. à café', 'cuil. à soupe', 'cuil à soupe', 'pincee', 'pincée', 'tête', 'l']
+    juxtaposant_list = ['de', 'd\'', 'à']
+    def __init__(self, ref: str, name: str, ingredients_bill: dict):
         self.ref = ref
         self.name = name
-        self.ingredients = []
+        self.ingredients = Recipe.parse_ingredients_bill(ingredients_bill)
+
+    @classmethod
+    def parse_ingredients_bill(cls, ingredients_bill: dict):
+        ingredients = []
         for line in ingredients_bill:
             # print(line)
             unit = 'p'
             rest = line
-            for unit_item in Recipe.unit_list:
+            for unit_item in cls.unit_list:
                 if line.startswith(unit_item + ' '):
                     unit = unit_item
-                    rest = line[str(unit).__len__():].lstrip()
-                    for jxt_item in Recipe.juxtaposant_list:
+                    rest = line[len(str(unit)):].lstrip()
+                    for jxt_item in cls.juxtaposant_list:
                         if rest.startswith(jxt_item):
-                            rest = rest[str(jxt_item).__len__():].lstrip()
+                            rest = rest[len(str(jxt_item)):].lstrip()
                             break
                     break
-            self.ingredients.append((Ingredient(rest), ingredients_bill[line], unit))
-        self.instructions = instructions
+            ingredients.append((Ingredient(rest), ingredients_bill[line], unit))
+        return ingredients
 
     def __str__(self):
         return f"{self.name} ({self.ref})"
@@ -60,3 +67,9 @@ class Ingredient:
         # prefered_unit
         # possible_units
         # print( 'Created: name: ' + str(self.name))
+    def categorize(self):
+        r = requests.get('https://fr.wikipedia.org/w/index.php?search=beurre', allow_redirects=True)
+        selector = Selector(text=str(r.content))
+        selector.xpath('//h1/text()').r
+        
+
