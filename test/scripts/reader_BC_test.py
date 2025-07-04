f82cb19ec0dc5bfeb93215e1a72820bd1aad10a7
Fr13drich <frederic.saelens@gmail.com>
2025-07-04 16:51:50 +0200

    add missing module

--- test/scripts/reader_BC_test.py
+++ test/scripts/reader_BC_test.py
@@ -1,12 +1,11 @@
-import os
+"""test the ouput of read_the_book.py on one BC recipe"""
 import subprocess
 #BCp384
 p = subprocess.run('python.exe ./read_the_book.py ./test/',
                            stdout=subprocess.PIPE, check=True)
 
 def test_answer():
+    """test the ouput of read_the_book.py on one BC recipe"""
     with open(file='./data/recipe_files/BCp384.json', mode='r', encoding='utf-16') as recipe_file:
         with open(file='./test/BCp384.json', mode='r', encoding='utf-16') as test_result:
             assert recipe_file.read() == test_result.read()
-    # print(p.stdout)
-
