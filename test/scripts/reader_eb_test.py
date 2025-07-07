0bab37a03d78a26e337e4cb30dece5bc68212344
Fr13drich <frederic.saelens@gmail.com>
2025-07-07 13:25:13 +0200

    add test EB CG

---
+++ test/scripts/reader_eb_test.py
@@ -0,0 +1,11 @@
+"""test the ouput of read_the_book.py on one recipe"""
+import configparser
+import common
+
+config = configparser.ConfigParser()
+config.read('./config.cfg')
+
+INPUT_DIR = config['TEST']['EB_PICS']
+common.run_reader(INPUT_DIR)
+def test_answer():
+    common.compare()
