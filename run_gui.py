63e7d1ae78aed2a9315568000c5f2e5f35421dc9
Fr13drich <frederic.saelens@gmail.com>
2025-07-13 14:31:21 +0200

    lint

--- run_gui.py
+++ run_gui.py
@@ -1,14 +1,13 @@
 """run gui"""
 from gui import App
 
+app = App()
 
 def on_closing():
+    """Clean exit"""
     app.withdraw()
     app.quit()
 
 if __name__ == "__main__":
-    app = App()
     app.protocol("WM_DELETE_WINDOW", on_closing)
     app.mainloop()
-
-
